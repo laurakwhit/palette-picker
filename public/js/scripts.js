803d40e258d38609344dea2fc9236874825e9510
Laura Whitaker <laura.k.whitaker@gmail.com>
2018-10-01 21:48:25 -0600

    Make Gem class

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,21 +1,41 @@
+class Gem {
+  constructor() {
+    this.colors = this.generateColors();
+    this.setGemColors();
+  }
 
-const generateHexCode = () => {
+  generateColors() {
+    let colors = [];
+
+    for (let i = 0; i < 5; i++) {
+      colors.push(this.generateHexCode())
+    }
+    return colors;
+  }
+
+  generateHexCode() {
     const characters = "0123456789ABCDEF"; 
     let color = '#'; 
 
     for (let i = 0; i < 6; i++)  {
       color += characters[(Math.floor(Math.random() * 16))];
     }
-    return color;
+    return color; 
+  }
+
+  setGemColors() {
+    $('.top-left-triangle').css('border-bottom-color', this.colors[0]);
+    $('.top-left-triangle h4').text(this.colors[0]);
+    $('.top-center-triangle').css('border-top-color', this.colors[1]);
+    $('.top-center-triangle h4').text(this.colors[1]);
+    $('.top-right-triangle').css('border-bottom-color', this.colors[2]);
+    $('.top-right-triangle h4').text(this.colors[2]);
+    $('.bottom-left-triangle').css('border-top-color', this.colors[3]);
+    $('.bottom-left-triangle h4').text(this.colors[3]);
+    $('.bottom-right-triangle').css('border-top-color', this.colors[4]);
+    $('.bottom-right-triangle h4').text(this.colors[4]);
+  }
 }
 
-const setGemColors = () => {
-  $('.top-left-triangle').css('border-bottom-color', generateHexCode())
-  $('.top-center-triangle').css('border-top-color', generateHexCode())
-  $('.top-right-triangle').css('border-bottom-color', generateHexCode())
-  $('.bottom-left-triangle').css('border-top-color', generateHexCode())
-  $('.bottom-right-triangle').css('border-top-color', generateHexCode())
-}
-
-$(window).on('load', setGemColors)
-$('.left-side__new-gem-btn').on('click', setGemColors)
+$('.left-side__new-gem-btn').on('click', () => new Gem())
+$(window).on('load', () => new Gem())
