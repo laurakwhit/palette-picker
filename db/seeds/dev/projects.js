d7de1dbd871397fc0bd51a8c6f034f4921d4da65
Laura Whitaker <laura.k.whitaker@gmail.com>
2018-10-03 19:43:07 -0600

    Seed db

---
+++ db/seeds/dev/projects.js
@@ -0,0 +1,21 @@
+
+exports.seed = function(knex, Promise) {
+  return knex('palettes').del() 
+    .then(() => knex('projects').del()) 
+
+    .then(() => {
+      return Promise.all([
+        knex('projects').insert({
+          name: 'EXAMPLE PROJECT'
+        }, 'id')
+        .then(project => {
+          return knex('palettes').insert([
+            { name: 'EXAMPLE PALETTE', color1: '#668586', color2: '#82AEB1', color3: '#93C6D6', color4: '#A7ACD9', color5: '#9E8FB2', project_id: project[0] }
+          ])
+        })
+        .then(() => console.log('Seeding complete!'))
+        .catch(error => console.log(`Error seeding data: ${error}`))
+      ]) 
+    })
+    .catch(error => console.log(`Error seeding data: ${error}`));
+};
